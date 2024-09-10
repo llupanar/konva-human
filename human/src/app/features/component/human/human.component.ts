5c9f321d7762fe985d98806c9cd314b01dc5e0c7
Vera <shijion@mail.ru>
2024-09-10 10:36:05 +0300

    add component

---
+++ human/src/app/features/component/human/human.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-human',
+  standalone: true,
+  imports: [],
+  templateUrl: './human.component.html',
+  styleUrl: './human.component.css'
+})
+export class HumanComponent {
+
+}
