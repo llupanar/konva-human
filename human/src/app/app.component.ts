ca9984b2074dcb8cd941cd5dd5d16b83b38f3f1d
Vera <shijion@mail.ru>
2024-09-10 10:59:53 +0300

    add main elements

--- human/src/app/app.component.ts
+++ human/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HumanComponent } from './features/component/human/human.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HumanComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- human/src/app/features/component/human/human.component.ts
+++ human/src/app/features/component/human/human.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import Konva from 'konva'
 
 @Component({
   selector: 'app-human',
@@ -7,6 +8,32 @@ import { Component } from '@angular/core';
   templateUrl: './human.component.html',
   styleUrl: './human.component.css'
 })
-export class HumanComponent {
+export class HumanComponent implements OnInit, AfterViewInit{
 
+  @ViewChild('canvasContainer', { static: true }) containerRef!: ElementRef;
+  stage!: Konva.Stage;
+  layer!: Konva.Layer;
+  head!: Konva.Circle;
+  body!: Konva.Line
+  leftArm!: Konva.Line;
+  rightArm!: Konva.Line;
+  leftLeg!: Konva.Line;
+  rightLeg!: Konva.Line;
+  leftElbow!: Konva.Circle;
+  rightElbow!: Konva.Circle;
+  leftHand!: Konva.Circle;
+  rightHand!: Konva.Circle;
+  leftKnee!: Konva.Circle;
+  rightKnee!: Konva.Circle;
+  leftFoot!: Konva.Circle;
+  rightFoot!: Konva.Circle;
+
+
+  ngOnInit(): void {
+    
+  }
+
+  ngAfterViewInit(): void {
+    
+  }
 }
