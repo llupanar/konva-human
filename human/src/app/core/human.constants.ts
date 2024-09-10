8834a6039b3ded73407e6ee96243ee908d843209
Vera <shijion@mail.ru>
2024-09-10 11:15:34 +0300

    draw head and body

---
+++ human/src/app/core/human.constants.ts
@@ -0,0 +1,8 @@
+export const  HEAD_X = 450;
+export const HEAD_Y = 100;
+
+export const BODY_START_X = HEAD_X;
+export const BODY_START_Y = HEAD_Y + 30; 
+export const BODY_END_X = HEAD_X;
+export const BODY_END_Y = 300;
+
--- human/src/app/features/component/human/human.component.ts
+++ human/src/app/features/component/human/human.component.ts
@@ -1,4 +1,5 @@
 import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import * as dots from '../../../core/human.constants'
 import Konva from 'konva'
 
 @Component({
@@ -14,7 +15,7 @@ export class HumanComponent implements OnInit, AfterViewInit{
   stage!: Konva.Stage;
   layer!: Konva.Layer;
   head!: Konva.Circle;
-  body!: Konva.Line
+  body!: Konva.Line;
   leftArm!: Konva.Line;
   rightArm!: Konva.Line;
   leftLeg!: Konva.Line;
@@ -28,12 +29,39 @@ export class HumanComponent implements OnInit, AfterViewInit{
   leftFoot!: Konva.Circle;
   rightFoot!: Konva.Circle;
 
-
   ngOnInit(): void {
-    
+    this.stage = new Konva.Stage({
+      container: this.containerRef.nativeElement,
+      width:800,
+      height:800
+    })
+    this.layer = new Konva.Layer();
+    this.stage.add(this.layer)
   }
 
   ngAfterViewInit(): void {
+    this.drawHuman()
+  }
+
+  drawHuman(){
+    this.head = new Konva.Circle({
+      x: dots.HEAD_X,
+      y: dots.HEAD_Y,
+      radius: 30,
+      fill: 'black',
+      stroke: 'gray',
+      strokeWidth: 2
+    })
+
+    this.body = new Konva.Line({
+      points:[dots.BODY_START_X, dots.BODY_START_Y, dots.BODY_END_X, dots.BODY_END_Y],
+      stroke: 'black',
+      strokeWidth:5
+    })
+
+    this.layer.add(this.head)
+    this.layer.add(this.body)
     
+    this.layer.draw();
   }
 }
